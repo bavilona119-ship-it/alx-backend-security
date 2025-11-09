bfd6d5087822a8d24e45d0ed6011edc728731987
bavilona119-ship-it <bavilona119@gmail.com>
2025-11-09 18:48:22 +0300

    Update middleware.py

--- ip_tracking/ip_tracking/middleware.py
+++ ip_tracking/ip_tracking/middleware.py
@@ -1,28 +1,28 @@
 # ip_tracking/middleware.py
 
-from .models import RequestLog
-from datetime import datetime
+from django.http import HttpResponseForbidden
+from .models import RequestLog, BlockedIP
 
 class IPLoggingMiddleware:
-    """Middleware to log the IP, timestamp, and path of each request."""
+    """Middleware to log requests and block blacklisted IPs."""
 
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Get the IP address
+        # Get client IP
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
             ip = x_forwarded_for.split(',')[0]
         else:
             ip = request.META.get('REMOTE_ADDR')
 
-        # Log the request
-        RequestLog.objects.create(
-            ip_address=ip,
-            path=request.path
-        )
+        # Check blacklist
+        if BlockedIP.objects.filter(ip_address=ip).exists():
+            return HttpResponseForbidden("Your IP is blocked.")
 
-        # Continue processing the request
+        # Log request
+        RequestLog.objects.create(ip_address=ip, path=request.path)
+
         response = self.get_response(request)
         return response
